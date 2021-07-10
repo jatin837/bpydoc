e518a7d241b07dfa55e28e74e72196179d36dabb
dj4t9n <pandeyjatin47@gmail.com>
2021-07-10 12:46:19 +0530

    regx to get dependency name and version

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 def read_requirements():
+    import re
+    p = re.compile("(\w*(\-\w*)*)\=\=(\d*\.\d*\.\d*)")
     with open("requirements.txt", 'r') as f:
         reqs = f.readlines()
 
+    res = []
+    ver = []
     for req in reqs:
-        req = req[:-1]
+        m = p.match(req)
+        res.append(m.group(1))
+        ver.append(m.group(3))
+
+    return res, ver
 
 
+read_requirements()
